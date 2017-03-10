bb7b3eff8a5a12542d2a12d23040f2cef9ef35e1
Adrian Jagodziński <obconiasty@hotmail.com>
2017-03-10 01:11:52 +0100

    Prosty preview wrzucocnych obrazków w dyrektywie

--- MyCloud/wwwroot/js/appPanel.js
+++ MyCloud/wwwroot/js/appPanel.js
@@ -2,7 +2,7 @@
     'use strict';
 
     angular.module('appPanel', ['ngRoute', 'angularFileUpload'])
-        .config(function ($routeProvider) {
+        .config(function($routeProvider) {
 
             $routeProvider.when("/",
             {
@@ -17,5 +17,37 @@
                 controllerAs: "vm",
                 templateUrl: "/views/filesView.html"
             });
-        });
+        })
+        .directive('ngThumb',
+        [
+            '$window', function ($window) {
+                return {
+                    restrict: 'A',
+                    template: '<canvas/>',
+                    link: function (scope, element, attributes) {
+
+                        var params = scope.$eval(attributes.ngThumb);
+
+                        var canvas = element.find('canvas');
+                        var reader = new FileReader();
+
+                        reader.onload = onLoadFile;
+                        reader.readAsDataURL(params.file);
+
+                        function onLoadFile(event) {
+                            var img = new Image();
+                            img.onload = onLoadImage;
+                            img.src = event.target.result;
+                        }
+
+                        function onLoadImage() {
+                            var width = params.width || this.width / this.height * params.height;
+                            var height = params.height || this.height / this.width * params.width;
+                            canvas.attr({ width: width, height: height });
+                            canvas[0].getContext('2d').drawImage(this, 0, 0, width, height);
+                        }
+                    }
+                }
+            }
+        ]);
 })();
