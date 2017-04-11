dbdee7721921bf09d5097f63bd1f9a89fe6acc37
Adrian Jagodziński <obconiasty@hotmail.com>
2017-04-12 00:57:31 +0200

    Częściowy front pod wieloelementowe zaznaczenie

---
+++ MyCloud/wwwroot/js/uiRemoveFiles.js
@@ -0,0 +1,11 @@
+﻿(function () {
+    var opacity = 0.3;
+
+    $("canvas")
+        .css('opacity', 0.3)
+        .off('click')
+        .on('click',
+            function() {
+                $(this).toggleClass('selected-canvas-opacity');
+            });
+})();
